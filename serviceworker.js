9b8de8d93daf02f9816bc2b5e9c1c0ce811e9ad6
Diksha126 <62324089+Diksha126@users.noreply.github.com>
2022-12-07 15:14:38 +0530

    Update serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,3 @@
-importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
-
+// Add this line to the top of the file
+  importScripts("https://cdn.moengage.com/webpush/releases/serviceworker_cdn.min.latest.js");
 
